32012705a859bddd94fc33c4ee1f67d689acb8c1
obaibuz <abaybuz@ukr.net>
2021-09-09 18:27:49 +0300

    Initial Commit

---
+++ Input By QR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Input By QR
+//
+//  Created by Baibuz Oleksandr on 09.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
